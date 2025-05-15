385fcb567703a60fa7030139feafad16e287d072
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-05-15 17:00:05 +0000

    qd tests and changes

--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,15 +1,10 @@
 #write functions here, don't add input('') statements here!
-def table(list1,list2):
+def table(listx,listy):
     table= ''
     temp= ''
-    for item1 in list1:
-        for item2 in list2:
-            temp+= str(item1 * item2)+' '
+    for itemy in listy:
+        for itemx in listx:
+            temp+= str(itemy * itemx)+' '
         table+= temp +'\n'
         temp= ''
     return table
-
-
-list1= [1,2,3,4,5,6,7,8,9,10]
-list2= [1,2,3,4,5,6,7,8,9,10]
-print(table(list1,list2))
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -3,10 +3,19 @@ import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
 from src.question_a.question_a import test_config
+#from src.question_b.question_b import
+#from src.question_c.question_c import 
+from src.question_d.question_d import table
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
-
+    def test_question_b_config(self):
+        self.assertEqual(True, test_config())
+    def test_question_c_config(self):
+        self.assertEqual(True, test_config())
+    def test_question_d_config(self):
+        self.assertEqual(table([1,2,3],[1,2,3]),'1 2 3 \n2 4 6 \n3 6 9 \n')
+        self.assertEqual(table([1,2,3],[1]),'1 2 3 \n')
 
