cfcd1e0e69b11f8174c4189b7aa8fb267ee1da21
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-05-16 22:11:39 +0000

    qc finished?

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,17 @@
 #add import
+from question_c import get_stock_list
+
+def qc_menu():
+    choice = 0
+    while choice ==0:
+        choice = int(input("Stock List Menu\n1 Display Stock Purchase History-\n2-Exit\n"))
+        while choice == 1:
+            stocklist= get_stock_list()
+            for stock in stocklist:
+                print(stock[0]+' '+stock[1]+'\n')
+            choice = 0
+        if choice == 2:
+            print("Exiting Program...")
+            exit
+
+qc_menu()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,25 @@
 #write functions here, don't add input('') statements here!
+
+class Stocks:
+    def __init__(self, symbol, company_name):
+        self.symbol= symbol
+        self.company_name= company_name
+    def get_symbol(self):
+        return self.symbol
+    def get_company_name(self):
+        return self.company_name
+
+
+
+def get_stock_list():
+    stockdata= [Stocks('AAPL','Apple Inc'),Stocks('CAT','Caterpillar'),Stocks('EK','Eastman Kodak'),Stocks('GOOG','Google'),Stocks('MSFT','Microsoft')]
+    stocklist= []
+    templist= []
+    for item in stockdata:
+        stock=item
+        templist.append(item.symbol)
+        templist.append(item.company_name)
+        stocklist.append(templist)
+        templist= []
+    return stocklist
+
